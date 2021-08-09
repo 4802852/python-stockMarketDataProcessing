37926f39dbed34538de0012247b9a8337fe0711b
4802852 <4802852@gmail.com>
2021-08-10 00:34:56 +0900

    minor change

--- InfBuy/data_process.py
+++ InfBuy/data_process.py
@@ -7,27 +7,27 @@ from slack.slack import *
 def data_open_rsi(filename):
     global path
     today = datetime.datetime.now().strftime("%Y%m%d")
-    df = pd.read_csv(path + '/' + filename)
+    df = pd.read_csv(path + "/" + filename)
     try:
-        df.insert(4,today,0)
+        df.insert(4, today, 0)
     except:
         pass
     for i in range(len(df)):
-        df.loc[i, today], df.loc[i, 'price'] = get_rsi(df.loc[i, 'symbol'])
-        if float(df.loc[i, 'base_rsi']) >= float(df.loc[i, today]):
-            df.loc[i, 'go'] = True
+        df.loc[i, today], df.loc[i, "price"] = get_rsi(df.loc[i, "symbol"])
+        if float(df.loc[i, "base_rsi"]) >= float(df.loc[i, today]):
+            df.loc[i, "go"] = True
         else:
-            df.loc[i, 'go'] = False
-    df.to_csv(path + '/' + filename, index=False)
-    is_go = df['go'] == True
+            df.loc[i, "go"] = False
+    df.to_csv(path + "/" + filename, index=False)
+    is_go = df["go"] == True
     to_go = df[is_go]
     go_list = to_go.values.tolist()
     for line in go_list:
-        info1 = f'{line[0]} 종목 RSI {line[4]} 기준 RSI {line[3]} 매수 가능'
-        info2 = f'현재가 ${line[2]} 40분할 * 2주 최소 투자금액 ${float(line[2]) * 40 * 2} 약 {int(float(line[2]) * 40 * 2 * 1100)}원'
+        info1 = f"{line[0]} 종목 RSI {line[4]} 기준 RSI {line[3]} 매수 가능"
+        info2 = f"현재가 ${line[2]} 40분할 * 2주 최소 투자금액 ${float(line[2]) * 40 * 2} 약 {int(float(line[2]) * 40 * 2 * 1100)}원"
         to_slack(info1)
         to_slack(info2)
 
 
 path = os.path.dirname(os.path.abspath(__file__))
-data_open_rsi('symbol_list.csv')
+data_open_rsi("symbol_list.csv")
--- InfBuy/exchangerateget.py
+++ InfBuy/exchangerateget.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 
 
 def get_exchange_rate():
-    url = 'https://finance.daum.net/exchanges'
+    url = "https://finance.daum.net/exchanges"
     raw = requests.get(url)
-    soup = BeautifulSoup(raw.text, 'html.parser')
-    rate = soup.find('#boxContents > div.tableB > div.box_contents > div > table > tbody > tr.first > td:nth-child(3) > span')
+    soup = BeautifulSoup(raw.text, "html.parser")
+    rate = soup.find(
+        "#boxContents > div.tableB > div.box_contents > div > table > tbody > tr.first > td:nth-child(3) > span"
+    )
     print(rate)
 
 
